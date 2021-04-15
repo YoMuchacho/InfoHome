01ab4a65dc489f60b09f0074598756476a825c9c
YoMuchacho <moisesvilladiegos@gmail.com>
2021-04-14 19:13:17 -0500

    Login redirect tabs

--- InfoHome/src/app/app.module.ts
+++ InfoHome/src/app/app.module.ts
@@ -7,14 +7,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { File } from '@ionic-native/file/ngx'
+import { File } from '@ionic-native/file/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageModule } from './pages/login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ComponentesModule, PublicacionCasaPageModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ComponentesModule,
+    PublicacionCasaPageModule,
+    HttpClientModule,
+    LoginPageModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ImagePicker, File],
   bootstrap: [AppComponent],
 })
